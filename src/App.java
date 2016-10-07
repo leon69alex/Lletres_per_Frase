9c237f0d1bbe6c2b5016de1003e293eb1b27e4d8
Alex Leon <leon69alex@gmail.com>
2016-10-07 19:23:49 +0200

    Commit1

---
+++ src/App.java
@@ -0,0 +1,23 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		String frase = "dwabniubudwaibii";
+		String[] array = frase.split("");
+		String[] lletres = new String[array.length];
+		
+		for(int i=0;i<array.length-1;i++){
+			int contador = 1;
+			for(int j=i+1;j<array.length;j++){
+
+				if(array[i].equals(array[j])){
+					contador ++;
+				}
+			}
+			System.out.println(array[i] + ":" + contador);
+		}
+
+	}
+
+}
